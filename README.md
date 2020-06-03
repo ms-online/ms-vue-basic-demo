36f7762756296301546613d9a6a28350f462668a
ms-online <27732357@qq.com>
2020-06-03 16:26:38 +0800

    lesson3-5

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-vue 入门课程学习
+lesson3:el & data 的使用
+lesson4:methods 方法
+lesson5:v-bind 绑定&v-htmls
---
+++ app.js
@@ -0,0 +1,21 @@
+// 实力化vue 对象
+new Vue({
+  el: '#vue-app', //html入口
+  data: {
+    name: 'Summer',
+    wechat: 123456,
+    website: 'https://www.baidu.com',
+    websiteTag: '<a href="https://www.taobao.com">淘宝</a>',
+  },
+  methods: {
+    // greeting() {
+    //   return '很高兴认识大家！';
+    // },
+    greeting(time) {
+      return `Good ${time} ${this.name}`;
+    },
+    haveLunch(time) {
+      return `${this.name} 你吃${time}了吗？`;
+    },
+  },
+});
