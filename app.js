a9bba2eb61c4c90c7d035d23778ec823b68012e2
ms-online <27732357@qq.com>
2020-07-02 18:05:06 +0800

    lesson18

--- app.js
+++ app.js
@@ -1,38 +1,35 @@
+// 全局变量（同时改变名称）
+
+let data = {
+  name: 'Yellemy',
+  age: 25,
+};
+
+// 创建全局组件
+Vue.component('greet', {
+  // html模版
+  template: `<p>这是全局组件，可以在任何实例的容器中使用
+    我的名字是：{{name}}, 我的年龄是：{{age}};
+    <button @click= changeName()>改变姓名</button>
+  </p>`,
+
+  // 属性
+  data() {
+    return data;
+  },
+  // 方法
+  methods: {
+    changeName() {
+      this.name = 'Worley';
+    },
+  },
+});
+
 // 实力化vue 对象
 const one = new Vue({
   el: '#vue-app-one', //html入口
-  data() {
-    return {
-      title: '这是app one 的title',
-    };
-  },
-  methods: {},
-  computed: {
-    greeting() {
-      return '你好，Yellemy！';
-    },
-  },
 });
 
 const two = new Vue({
   el: '#vue-app-two', //html入口
-  data() {
-    return {
-      title: '这是app two 的title',
-    };
-  },
-  methods: {
-    // apptwo修改appone
-    changeAppOneTitle() {
-      one.title = 'Hello World！';
-    },
-  },
-  computed: {
-    greeting() {
-      return '你好，Worley！';
-    },
-  },
 });
-
-// 外部修改
-two.title = '哈哈哈';
